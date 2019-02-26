ed932d3740b50f4f700f91f3daadfd2a1b0c389a
Hanson Ng <ng.hanson@hotmail.com>
2019-02-26 00:11:24 -0500

    Refactoring/Renaming

--- TeensyMain/IMUTest.ino
+++ TeensyMain/IMUTest.ino
@@ -3,22 +3,16 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
-//extern TwoWire Wire2;
+
 Adafruit_BNO055 bno = Adafruit_BNO055();
 
-void setup() {
-//Wire2.begin();
-//  CORE_PIN18_CONFIG = 0;  // turn off primary pins before enable alternates
-//  CORE_PIN19_CONFIG = 0;
-//  CORE_PIN4_CONFIG = PORT_PCR_MUX(2)|PORT_PCR_ODE|PORT_PCR_SRE|PORT_PCR_DSE;
-//  CORE_PIN3_CONFIG = PORT_PCR_MUX(2)|PORT_PCR_ODE|PORT_PCR_SRE|PORT_PCR_DSE;
-  // put your setup code here, to run once:
+void imuTestSetup() {
+
   Serial.begin(115200);
   Serial.println("Orientation Sensor Test");
   /* Initialise the sensor */
   if(!bno.begin())
   {
-    /* There was a problem detecting the BNO055 ... check your connections */
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while(1);
   }
@@ -34,7 +28,7 @@ void setup() {
 }
 
 int count = 0;
-void loop() {
+void imuTestLoop() {
   sensors_event_t event; 
   bno.getEvent(&event);
   
